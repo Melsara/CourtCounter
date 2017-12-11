86e274c1df27f28cbc4e74b09d12ee6301e7acb0
Melsara <kaxhortz@gmail.com>
2017-12-11 22:04:02 +0100

    scoreA defined as global variable. Adding logic to the buttons.

--- app/src/main/java/com/sardegnaisoladicavalli/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/sardegnaisoladicavalli/example/courtcounter/MainActivity.java
@@ -2,12 +2,42 @@ package com.sardegnaisoladicavalli.example.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int scoreA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(scoreA);
+
     }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int scoreA) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(scoreA));
+    }
+
+    public void addThreePoints (View view) {
+        scoreA = scoreA +3;
+        displayForTeamA(scoreA);
+    }
+
+    public void addTwoPoints (View view) {
+        scoreA = scoreA +2;
+        displayForTeamA(scoreA);
+    }
+
+    public void addFreeThrow (View view) {
+        scoreA = scoreA +1;
+        displayForTeamA(scoreA);
+    }
+
 }
